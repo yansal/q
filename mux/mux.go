49bd72362dc367d8d6ef27e9ea7fb3a34db28720
Yann Salaün <yann.salaun@ulule.com>
2019-01-30 19:32:37 +0100

    add a form and a POST handler to publish to queues

--- mux/mux.go
+++ mux/mux.go
@@ -17,7 +17,7 @@ func New(q q.Q) (*http.ServeMux, error) {
 	}
 
 	mux := http.NewServeMux()
-	mux.Handle("/", handleError(handler(q, template)))
+	mux.Handle("/", &handler{q: q, template: template})
 	return mux, nil
 }
 
@@ -32,13 +32,44 @@ func handleError(h handlerFunc) http.HandlerFunc {
 	}
 }
 
-func handler(q q.Q, template *template.Template) handlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) error {
-		ctx := r.Context()
-		stats, err := q.Stats(ctx)
-		if err != nil {
-			return err
-		}
-		return errors.WithStack(template.Execute(w, stats))
+type handler struct {
+	q        q.Q
+	template *template.Template
+}
+
+func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	handleError(h.serveHTTP)(w, r)
+}
+
+func (h *handler) serveHTTP(w http.ResponseWriter, r *http.Request) error {
+	switch r.Method {
+	case http.MethodGet:
+		return h.serveGET(w, r)
+	case http.MethodPost:
+		return h.servePOST(w, r)
+	default:
+		status := http.StatusMethodNotAllowed
+		http.Error(w, http.StatusText(status), status)
+		return nil
 	}
+}
+
+func (h *handler) serveGET(w http.ResponseWriter, r *http.Request) error {
+	ctx := r.Context()
+	stats, err := h.q.Stats(ctx)
+	if err != nil {
+		return err
+	}
+	return errors.WithStack(h.template.Execute(w, stats))
+}
+
+func (h *handler) servePOST(w http.ResponseWriter, r *http.Request) error {
+	ctx := r.Context()
+	queue := r.FormValue("queue")
+	payload := r.FormValue("payload")
+	if err := h.q.Publish(ctx, queue, payload); err != nil {
+		return err
+	}
+	http.Redirect(w, r, "", http.StatusFound)
+	return nil
 }
